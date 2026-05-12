5450934ebd4621c8c9a4b4ebf578402dcec43f89
Daniel Schuh <Nehil1984@users.noreply.github.com>
2026-05-12 09:56:36 +0200

    fix: avoid strict mode double init

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 )
